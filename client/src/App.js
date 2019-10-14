fc421532c6de959c11b6ac156130958089357e65
Joshua <cowley.josh@gmail.com>
2019-10-13 22:45:21 -0400

    MVP Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./Routes/PrivateRoute"
+import BubblesPage from "./components/BubblePage"
 
 function App() {
-  const [colorList, setColorList] = useState([]);
+  //const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubbles" component={BubblesPage}/>
       </div>
     </Router>
   );
